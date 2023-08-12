91473a0e8e3306dbda1faf3fbc8545f3672d971d
PointOfKnowReturn <cygnus@cygnusxone.com>
2023-08-11 21:25:42 -0500

    Added a test for Vehicle Checker
    Works for past 48 hours but has place to change that

--- config.py
+++ config.py
@@ -12,41 +12,42 @@ def get_account_details(account_number):
     - account_number (int): The number of the account (e.g., 1 for the first account, 2 for the second, etc.)
 
     Returns:
-    - dict: A dictionary containing the client_id, client_secret, and auth_code for the specified account.
+    - dict: A dictionary containing the account_name, client_id, client_secret, and auth_code for the specified account.
     """
+    account_name_key = f"ACCOUNT_NAME_{account_number}"
     client_id_key = f"BOUNCIE_CLIENT_ID_{account_number}"
     client_secret_key = f"BOUNCIE_CLIENT_SECRET_{account_number}"
     auth_code_key = f"BOUNCIE_AUTH_CODE_{account_number}"
 
+    account_name = os.getenv(account_name_key)
     client_id = os.getenv(client_id_key)
     client_secret = os.getenv(client_secret_key)
     auth_code = os.getenv(auth_code_key)
 
-    if not all([client_id, client_secret, auth_code]):
+    if not all([account_name, client_id, client_secret, auth_code]):
         raise ValueError(f"Details for account {account_number} not found or incomplete in .env file.")
 
     return {
+        "account_name" : account_name,
         "client_id": client_id,
         "client_secret": client_secret,
         "auth_code": auth_code
     }
 
-def get_all_accounts():
+def get_total_accounts():
     """
-    Retrieve the details of all accounts present in the .env file.
+    Retrieve the total number of accounts present in the .env file based on the presence of BOUNCIE_CLIENT_ID_X keys.
 
     Returns:
-    - list: A list of dictionaries, each containing the client_id, client_secret, and auth_code for an account.
+    - int: The total number of accounts.
     """
-    accounts = []
     account_number = 1
-
     while True:
-        try:
-            account_details = get_account_details(account_number)
-            accounts.append(account_details)
+        client_id_key = f"BOUNCIE_CLIENT_ID_{account_number}"
+        if os.getenv(client_id_key):
             account_number += 1
-        except ValueError:
+        else:
             break
+    return account_number - 1
 
-    return accounts
+#
--- tests/test_vehicle_checker.py
+++ tests/test_vehicle_checker.py
@@ -1,3 +1,72 @@
-# This module will leverage the API client to retrieve information about vehicles and their trips.
-# Its primary function will be to identify vehicles that have remained stationary for the past week.
-# (This module will be developed further based on the specific requirements.)
+# inactivity_checker.py
+
+from api.client import BouncieClient
+from api.auth import get_access_token
+from config import get_total_accounts, get_account_details
+import datetime
+
+def check_vehicle_inactivity(account_number, account_name, threshold_hours=48):
+    """
+    Check if any vehicles in the specified account have been inactive for more than the threshold hours.
+
+    Args:
+    - account_number (int): The number of the account to check.
+    - threshold_hours (int, optional): The inactivity threshold in hours. Default is 48 hours.
+
+    Returns:
+    - list: A list of vehicle names that have been inactive for more than the threshold hours.
+    """
+    # Get access token for the specified account
+    access_token = get_access_token(account_number)
+
+    # Initialize the BouncieClient with the access token
+    bouncie_client = BouncieClient(access_token)
+
+    # Retrieve the list of vehicles
+    vehicles = bouncie_client.search_vehicles()
+
+    # List to store names of inactive vehicles
+    inactive_vehicles = []
+
+    for vehicle in vehicles:
+        imei = vehicle.get("imei")
+        # Retrieve the trips for the vehicle
+        trips = bouncie_client.search_trips(imei=imei, gps_format="polyline")
+        # If there are no trips, consider the vehicle as inactive
+        if not trips:
+            inactive_vehicles.append(f"{account_name} - {vehicle.get('nickName', 'Unknown Vehicle')}")
+            continue
+
+        # Get the endTime of the most recent trip
+        last_trip_end_time = trips[0].get("endTime")
+        if last_trip_end_time:
+            last_trip_end_time = datetime.datetime.fromisoformat(last_trip_end_time.replace("Z", "+00:00"))
+            current_time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+            time_difference = current_time - last_trip_end_time
+
+            # Check if the time difference exceeds the threshold
+            if time_difference.total_seconds() > threshold_hours * 3600:
+                inactive_vehicles.append(f"{account_name} - {vehicle.get('nickName', 'Unknown Vehicle')}")
+
+    return inactive_vehicles
+
+def main():
+    total_accounts = get_total_accounts()
+    all_inactive_vehicles = []
+
+    for account_number in range(1, total_accounts + 1):
+        account_details = get_account_details(account_number)
+        inactive_vehicles = check_vehicle_inactivity(account_number, account_details["account_name"])
+        all_inactive_vehicles.extend(inactive_vehicles)
+
+
+    if all_inactive_vehicles:
+        print("Vehicles inactive for more than 48 hours:")
+        for vehicle_name in all_inactive_vehicles:
+            print(vehicle_name)
+    else:
+        print("All vehicles have been active in the last 48 hours.")
+
+if __name__ == "__main__":
+    main()
+
